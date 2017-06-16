341d1b3a5129ed0cec9ba6f3d4bc418c5743c41f
keenubee <aleks.dev@gmail.com>
2017-06-16 23:02:12 +0300

    Init test app

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+
+import App from './App';
+import reducers from './reducers';
+
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+const store = compose(
+  applyMiddleware(createLogger())
+)(createStore)(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
---
+++ src/reducers/tasks.js
@@ -0,0 +1,14 @@
+// TODO: Add Tasks related reducers here:
+// We should be able to CRUD Task entities
+// Every Task should include:
+// 'task_id', 'task_title', 'task_description', 'deadline' and 'assignee' (some percon's name)
+
+const initialState = {
+    tasks : []
+};
+
+const tasks = (state = initialState, action) => {
+  return state;
+};
+
+export default tasks;
